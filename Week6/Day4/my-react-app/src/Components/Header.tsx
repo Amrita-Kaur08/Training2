b32e0d6e14a460e26e2395697eb3e5bcc698e80f
Amrita <amrita083@gmail.com>
2025-03-24 14:31:43 +0530

    Added W6_Day4

---
+++ Week6/Day4/my-react-app/src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ padding: "10px", background: "#282c34", color: "white" }}>
+      <h1>My React App</h1>
+    </header>
+  );
+};
+
+export default Header;
